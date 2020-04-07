f296e0e2fcb61d895cd106868d6a59a4378b3364
mpooley <mpooley@exponent.com>
2020-04-07 16:24:41 -0400

    hand over to Nick

--- stayWell/stayWellCore/views.py
+++ stayWell/stayWellCore/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+class HomeView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """ Return the last five published questions. """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date').exclude(choice=None)[:5]
