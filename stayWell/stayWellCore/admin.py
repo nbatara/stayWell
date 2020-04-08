31139dac9be270def2937ac00390d659e4579573
Nicolas A. Batara <nick.batara@gmail.com>
2020-04-08 17:30:27 -0400

    add model to admin site

--- stayWell/stayWellCore/admin.py
+++ stayWell/stayWellCore/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Question, QuestionCharField, QuestionDropDownField, SurveyEntry
+from .models import ChoiceDropDownField, QuestionCharField, QuestionDropDownField, SurveyEntry
 # Register your models here.
 admin.site.register(QuestionCharField)
 admin.site.register(QuestionDropDownField)
 admin.site.register(SurveyEntry)
+admin.site.register(ChoiceDropDownField)
+
